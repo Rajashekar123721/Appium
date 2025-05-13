182534009178a62b47b27908e823d5fa5379a9fb
Rajashekar123721 <g.rajashekarreddy123@gmail.com>
2025-05-13 15:50:48 +0530

    initial commit

---
+++ src/test/java/rahulshettyacademy/DragAndDropDemo.java
@@ -0,0 +1,33 @@
+package rahulshettyacademy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.appium.java_client.AppiumBy;
+
+public class DragAndDropDemo extends BaseTest{
+
+
+	@Test 
+	public void DragAndDropTest() throws InterruptedException {
+		driver.findElement(AppiumBy.accessibilityId("Views")).click();
+		driver.findElement(AppiumBy.accessibilityId("Drag and Drop")).click();
+		
+		WebElement source=driver.findElement(By.id("io.appium.android.apis:id/drag_dot_1"));
+		((JavascriptExecutor) driver).executeScript("mobile: dragGesture",
+				ImmutableMap.of("elementId", ((RemoteWebElement) source).getId(),
+						"endX", 714,
+						"endY", 714
+						));
+		
+		Thread.sleep(5000);
+		String dropmsg=driver.findElement(By.id("io.appium.android.apis:id/drag_result_text")).getText();
+		Assert.assertEquals(dropmsg,"Dropped!");
+	}
+}
